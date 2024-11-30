8c958faed38333ea8113c7b0a0302c0d21704da5
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-11-30 22:53:04 +0530

    second largest in array

--- 1_second_largest_in_array.java
+++ 1_second_largest_in_array.java
@@ -1,3 +1,4 @@
+//O(nlogn) time
 class Solution {
     public int getSecondLargest(int[] arr) {
         // Code Here
@@ -7,6 +8,47 @@ class Solution {
             if(arr[n-2]!=arr[n-1]) return arr[i];
         }
         return -1;
+    }
+}
+
+
+//O(n) time
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // Code Here
+        int n = arr.length;
+        int largest = -1, secondlargest = -1;
+        for(int i =0 ;i<n;i++){
+            if(arr[i]>largest){
+                largest=arr[i];
+            }
+        }
+        for(int i =0;i<n;i++){
+            if(arr[i]>secondlargest && arr[i]!=largest){
+                secondlargest=arr[i];
+            }
+        }
+        return secondlargest;
+    }
+}
+
+
+//O(n) time and best approach in one pass only
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // Code Here
+        int n = arr.length;
+        int largest = -1, secondLargest = -1;
+        for (int i = 0; i < n; i++) {
+            if(arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i] < largest && arr[i] > secondLargest) {
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
     }
 }
 
